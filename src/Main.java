428c9b5f18d38f48ff04fafe0e5988533d91c2ad
Ян <peroff007@gmail.com>
2024-03-27 01:57:53 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Задача 1:");
+        double totalMoney = 0;
+        short moneyPerMount = 15000;
+        int targetMoney = 2459000;
+        int month = 0;
+        double rate = 0;
+        while (totalMoney < targetMoney){
+            rate = (totalMoney / 100) * 1;
+            totalMoney = totalMoney + rate + moneyPerMount;
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalMoney + " рублей");
         }
+        System.out.println("Для накопления суммы " + totalMoney + " рублей понадобилось " + month + " месяцев");
     }
 }
